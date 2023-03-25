8cd7218b37fbe97d4e18accf3a61d8c759593b61
ivankoros <ivan.koros2@gmail.com>
2023-03-25 01:26:58 -0400

    added proper file validations for dimensions with no global variables, added todo for a "dup" syntax function (necessary for this to run), changed functions to only have to read incoming Excel files into dataframes once instead of at every junction. updated upload button to always be avalible for now

--- custom_tkint.py
+++ custom_tkint.py
@@ -8,41 +8,41 @@ from functions import *
 
 
 # Browse and upload files
-def browse_files(entry_type, raw_sample, raw_sample_diagram):
+def browse_files(entry_type, raw_sample, raw_sample_diagram, **kwargs):
+
+
+    # Browse for .xlsx and .xls files only
     filepath = filedialog.askopenfilename(filetypes=(("Excel files", "*.xlsx;*.xls"), ("All files", "*.*")))
+
     if filepath and is_excel(filepath):
         if entry_type == "raw":
             raw_sample: str = filepath
-            message, color = dimension_validation(filepath, 96, 16)
+            raw_sample_df = pd.read_excel(raw_sample)
+            message, color, bool_result_raw = dimension_validation(raw_sample_df, 96, 16)
             raw_status_label.configure(text=message, fg_color=color)
+
+            return raw_sample, raw_sample_df
+
         elif entry_type == "diagram":
             raw_sample_diagram: str = filepath
-            message, color = dimension_validation(filepath, 8, 13)
+            raw_sample_diagram_df = pd.read_excel(raw_sample_diagram)
+            message, color, bool_result_diagram = dimension_validation(raw_sample_diagram_df, 8, 13)
             diagram_status_label.configure(text=message, fg_color=color)
+
+            return raw_sample, raw_sample_diagram_df
+
+
+        # if bool_result_raw and bool_result_diagram:
+        #     upload_button.configure(state="normal")
+        # else:
+        #     upload_button.configure(state="disabled")
 
     elif filepath:
         showerror("Invalid file type", "Please select Excel files (.xls or .xlsx) only.")
-        return raw_sample, raw_sample_diagram
-
-    if raw_sample and raw_sample_diagram:
-        upload_button.configure(state="normal")
-    else:
-        upload_button.configure(state="disabled")
 
     return raw_sample, raw_sample_diagram
 
-def upload_files(raw_sample, raw_sample_diagram):
 
-    # Read data
-    df_data = pd.read_excel(raw_sample, sheet_name=0)
-    df_diagram = pd.read_excel(raw_sample_diagram)
-
-    # TODO add preprocessing code here
-
-    # Activate download button
-    download_button.configure(state="normal")
-
-    return raw_sample, raw_sample_diagram
 
 
 def download_file():
@@ -80,18 +80,17 @@ raw_status_label = ctk.CTkLabel(frame, text="", width=10, height=1)
 raw_status_label.grid(row=1, column=0, pady=10)
 
 
-raw_sample = ""
-raw_sample_diagram = ""
+
 # File upload browse buttons
 raw_browse_button = ctk.CTkButton(frame,
                                   text="Upload raw sample file",
-                                  command=lambda: browse_files("raw", "", ""))
+                                  command=lambda: browse_files("raw", "", "", bool_result_raw=False))
 
 raw_browse_button.grid(row=0, column=0, padx=10)
 
 diagram_browse_button = ctk.CTkButton(frame,
                                       text="Upload plate diagram",
-                                      command=lambda: browse_files("diagram", "", ""))
+                                      command=lambda: browse_files("diagram", "", "", bool_result_diagram=False))
 
 diagram_browse_button.grid(row=0, column=1, padx=10)
 
@@ -106,8 +105,9 @@ download_button.pack(pady=(10, 20), side="bottom", anchor="center")
 # File upload button, disabled by default until both files uploaded
 upload_button = ctk.CTkButton(window,
                               text="Upload",
-                              state="disabled",
+                              state="enabled",
                               command=upload_files)
+
 
 upload_button.pack(side="bottom", anchor="center")
 
--- functions.py
+++ functions.py
@@ -5,26 +5,42 @@ def is_excel(filename):
     return filename.lower().endswith(('.xls', '.xlsx'))
 
 
-def dimension_validation(filename, expected_rows, expected_columns):
+def dimension_validation(file, expected_rows, expected_columns):
     dimension_validation_result: str = ""
     dimension_validation_color = ""
 
-    df = pd.read_excel(filename)
-    rows, columns = df.shape
+    rows, columns = file.shape
 
     if (rows, columns) != (expected_rows, expected_columns):
         dimension_validation_result = f"✗ File should be {expected_rows} x {expected_columns}, but is {rows} x {columns}"
         dimension_validation_color = "red"
+        dimension_validation_result_bool = False
     else:
         dimension_validation_result = "✓ File looks good"
         dimension_validation_color = "green"
+        dimension_validation_result_bool = True
 
-    return dimension_validation_result, dimension_validation_color
+    return dimension_validation_result, dimension_validation_color, dimension_validation_result_bool
+
+# TODO: add dup validation function here
+# Check if file is properly formatted to contain "dup" labels
+def dup_validation(file):
+    dup_validation_result: str = ""
+    dup_validation_color = ""
+
+    df = pd.read_excel(file)
+    dup = df.duplicated().any()
+
+    if dup:
+        dup_validation_result = "✗ File contains duplicate rows"
+        dup_validation_color = "red"
+    else:
+        dup_validation_result = "✓ File looks good"
+        dup_validation_color = "green"
+
+    return dup_validation_result, dup_validation_color
 
 
-        # df_data.shape, (96, 16),
-        #                      f"Raw data is {df_data.shape[0]} rows by {df_data.shape[1]} columns but should be 96 rows by 3 columns")
-        #
-        # def test_plate_diagram(self):
-        #     self.assertEqual(df_diagram.shape, (8, 13),
-        #                      f"Plate diagram is {df_diagram.shape[0]} rows by {df_diagram.shape[1]} columns but should be 8 rows by 13 columns")
+def upload_files_preprocessing(raw_sample_df, raw_sample_diagram_df):
+
+    pass
