172a4a47707a86ffeb78fc360348dd71d9bda5f8
ivankoros <ivan.koros2@gmail.com>
2023-03-25 00:41:00 -0400

    added function in functions.py to read uploaded excel files into pandas dataframes and perform dimension validation

--- functions.py
+++ functions.py
@@ -5,3 +5,26 @@ def is_excel(filename):
     return filename.lower().endswith(('.xls', '.xlsx'))
 
 
+def dimension_validation(filename, expected_rows, expected_columns):
+    dimension_validation_result: str = ""
+    dimension_validation_color = ""
+
+    df = pd.read_excel(filename)
+    rows, columns = df.shape
+
+    if (rows, columns) != (expected_rows, expected_columns):
+        dimension_validation_result = f"✗ File should be {expected_rows} x {expected_columns}, but is {rows} x {columns}"
+        dimension_validation_color = "red"
+    else:
+        dimension_validation_result = "✓ File looks good"
+        dimension_validation_color = "green"
+
+    return dimension_validation_result, dimension_validation_color
+
+
+        # df_data.shape, (96, 16),
+        #                      f"Raw data is {df_data.shape[0]} rows by {df_data.shape[1]} columns but should be 96 rows by 3 columns")
+        #
+        # def test_plate_diagram(self):
+        #     self.assertEqual(df_diagram.shape, (8, 13),
+        #                      f"Plate diagram is {df_diagram.shape[0]} rows by {df_diagram.shape[1]} columns but should be 8 rows by 13 columns")
